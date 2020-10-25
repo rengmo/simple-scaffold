c8a0c766b7ab7b5328660ef627de4a22e421af4c
renmo <to_see_the_world@163.com>
2020-10-25 12:17:42 +0800

    add babel plugins of ES class

--- .babelrc.js
+++ .babelrc.js
@@ -9,4 +9,9 @@ module.exports = {
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+  ],
 };
